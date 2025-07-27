852c7baf89f63b14def7b0846994676410519e2a
Luis Garrillo <lggarrillo@urbe.edu.ve>
2025-07-27 17:40:09 -0400

    Create basic components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,19 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className={styles.page}>
-      this is the epic page
+      <header>
+        <h1>
+          Game Dialogue Maker
+        </h1>
+        <select></select>
+        <h1>Manage your projects</h1>
+      </header>
+      <div>
+        The dialogue boxes go here
+      </div>
+      <div>
+
+      </div>
     </div>
   );
 }
